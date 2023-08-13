9ce9ce2bdfb3e7b76ed2b6c01c9c7d7e63438a0b
nahidath <36660735+nahidath@users.noreply.github.com>
2023-08-13 10:48:46 -0400

    adding genre component and get functions

---
+++ fyg/src/app/components/Genre.js
@@ -0,0 +1,12 @@
+import styles from '../css/genre.module.css';
+
+
+
+const Genre = ({id, name, image_url }) => {
+    return (
+        <div className={styles.genre} key={id} >
+            <img className={styles.imgGenre} src={image_url} alt=""/>
+            <div className={styles.genreName}>{name}</div>
+        </div>
+    )
+}
--- fyg/src/app/home.js
+++ fyg/src/app/home.js
@@ -1,10 +1,60 @@
 import styles from "./home.module.css";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {REACT_APP_API_KEY} from "@env";
+import Card from "@/app/components/Card";
 
 const Home1 = () => {
+  const [newGames, setNewGames] = useState([]);
+  const [popularGames, setPopularGames] = useState([]);
+  const [genres, setGenres] = useState([]);
+  let apiKey = REACT_APP_API_KEY;
 
-  const newGamesList = () => {
 
+  const getNewGamesList = () => {
+    //get the date of today with format YYYY-MM-DD
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0 so need to add 1 to make it 1!
+    let yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = "0" + dd;
+    }
+    if (mm < 10) {
+        mm = "0" + mm;
+    }
+    today = yyyy + "-" + mm + "-" + dd;
+    axios.get(`https://api.rawg.io/api/games`, {params:{apiKey: apiKey, ordering: "-relased", dates: "2023-01-01,"+today, page_size:3}})
+        .then((res) => {
+            setNewGames(res.data.results);
+            console.log(res.data.results);
+        }
+    );
   }
+
+  const getPopularGames = () => {
+    axios.get(`https://api.rawg.io/api/games`, {params:{apiKey: apiKey, ordering: "-rating", dates: "2023-01-01,"+today, page_size:3}})
+        .then((res) => {
+            setPopularGames(res.data.results);
+            console.log(res.data.results);
+        }
+    );
+  }
+
+  const getGenres = () => {
+    axios.get(`https://api.rawg.io/api/genres`, {params:{apiKey: apiKey}})
+        .then((res) => {
+            setGenres(res.data.results);
+            console.log(res.data.results);
+        }
+    );
+  }
+
+  useEffect(() => {
+    getNewGamesList();
+    getPopularGames();
+    getGenres();
+    }, []);
 
 
   return (
@@ -36,6 +86,10 @@ const Home1 = () => {
                 <div className={styles.txt}>More</div>
               </div>
               <div className={styles.inner} />
+              {/*{newGames.map((game) => (*/}
+              {/*  <Card id={game.id} name={game.name} image_url={game.background_image} platforms={game.platforms} />*/}
+              {/*))}*/}
+              {/*call card here*/}
               <div className={styles.child6} />
             </div>
           </div>
@@ -51,6 +105,9 @@ const Home1 = () => {
                 <div className={styles.txt}>More</div>
               </div>
               <div className={styles.inner} />
+              {/*{popularGames.map((game) => (*/}
+              {/*    <Card id={game.id} name={game.name} image_url={game.background_image} platforms={game.platforms} />*/}
+              {/*))}*/}
               <div className={styles.child6} />
             </div>
           </div>
